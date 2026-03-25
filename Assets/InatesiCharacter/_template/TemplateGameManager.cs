5f3942aacf745fb16170c714caa84e0eba6ae763
inatesi <rinat24mai2000@gmail.com>
2026-03-26 00:03:50 +0300

    26 03 26

---
+++ Assets/InatesiCharacter/_template/TemplateGameManager.cs
@@ -0,0 +1,16 @@
+using Inatesi.Game;
+using InatesiCharacter.Testing.LeoEcs5.Utility;
+using UnityEngine;
+
+
+namespace InatesiCharacter.Template
+{
+    public class TemplateGameManager : GameLogicBase
+    {
+        public override void OnPlayerSpawn()
+        {
+
+        }
+    }
+}
+
